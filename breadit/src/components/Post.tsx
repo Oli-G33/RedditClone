c3a143fa9cfe4e803cf352df2701632917b2dbad
OliGarc333 <ngarcia333@gmail.com>
2024-04-21 13:06:22 +0200

    Implementing votes

--- breadit/src/components/Post.tsx
+++ breadit/src/components/Post.tsx
@@ -1,8 +1,14 @@
-import { User } from '@prisma/client';
-import { FC, useRef } from 'react';
+'use client';
+
 import { formatTimeToNow } from '@/lib/utils';
+import { Post, User, Vote } from '@prisma/client';
 import { MessageSquare } from 'lucide-react';
+import Link from 'next/link';
+import { FC, useRef } from 'react';
 import EditorOutput from './EditorOutput';
+import PostVoteClient from './post-vote/PostVoteClient';
+
+type PartialVote = Pick<Vote, 'type'>;
 
 interface PostProps {
   post: Post & {
@@ -15,19 +21,24 @@ interface PostProps {
   commentAmt: number;
 }
 
-const Post: FC<PostProps> = ({
+const PostComponent: FC<PostProps> = ({
   post,
   votesAmt: _votesAmt,
   currentVote: _currentVote,
   subredditName,
   commentAmt
 }) => {
-  const pRef = useRef<HTMLDivElement>(null);
+  const pRef = useRef<HTMLParagraphElement>(null);
 
   return (
     <div className="bg-white rounded-md shadow">
       <div className="flex justify-between px-6 py-4">
-        {/*TODO: Votes*/}
+        <PostVoteClient
+          postId={post.id}
+          initialVotesAmt={_votesAmt}
+          initialVote={_currentVote?.type}
+        />
+
         <div className="flex-1 w-0">
           <div className="mt-1 text-xs text-gray-500 max-h-40">
             {subredditName ? (
@@ -41,37 +52,37 @@ const Post: FC<PostProps> = ({
                 <span className="px-1">•</span>
               </>
             ) : null}
-            <span>Posted by u/{post.author.name}</span>{' '}
+            <span>Posted by u/{post.author.username}</span>{' '}
             {formatTimeToNow(new Date(post.createdAt))}
           </div>
-          <a href={`/r/${subredditName}/post/${post.id}}`}>
+          <a href={`/r/${subredditName}/post/${post.id}`}>
             <h1 className="py-2 text-lg font-semibold leading-6 text-gray-900">
               {post.title}
             </h1>
           </a>
+
           <div
             className="relative w-full text-sm max-h-40 overflow-clip"
             ref={pRef}
           >
             <EditorOutput content={post.content} />
             {pRef.current?.clientHeight === 160 ? (
-              <div className="absolute bottom-0 left-0 h-24 bg-gradient-to-t from-white to-transparent" />
+              // blur bottom if content is too long
+              <div className="absolute bottom-0 left-0 w-full h-24 bg-gradient-to-t from-white to-transparent"></div>
             ) : null}
           </div>
         </div>
       </div>
 
-      <div className="z-20 p-4 bg-gray-50 sm:px-6">
-        {' '}
-        <a
+      <div className="z-20 px-4 py-4 text-sm bg-gray-50 sm:px-6">
+        <Link
+          href={`/r/${subredditName}/post/${post.id}`}
           className="flex items-center gap-2 w-fit"
-          href={`/r/${subredditName}/post/${post.id}}`}
         >
           <MessageSquare className="w-4 h-4" /> {commentAmt} comments
-        </a>
+        </Link>
       </div>
     </div>
   );
 };
-
-export default Post;
+export default PostComponent;
