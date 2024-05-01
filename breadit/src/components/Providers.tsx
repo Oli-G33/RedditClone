03b4d098d8499f62f4b4208e794930d35c8f5403
OliGarc333 <ngarcia333@gmail.com>
2024-05-01 13:35:48 +0200

    cleaned up imports

--- breadit/src/components/Providers.tsx
+++ breadit/src/components/Providers.tsx
@@ -2,15 +2,12 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SessionProvider } from 'next-auth/react';
-import { NextUIProvider } from '@nextui-org/react';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
-      <NextUIProvider>
-        <SessionProvider>{children}</SessionProvider>
-      </NextUIProvider>
+      <SessionProvider>{children}</SessionProvider>
     </QueryClientProvider>
   );
 };
